62fe2b40534bbb8a89decaae9d694e3fdee38c9e
FlorianMichael <jessica.reuth49@gmail.com>
2021-05-14 16:29:25 +0200

    Update Via

---
+++ src/main/java/de/enzaxd/viaforge/injection/mixins/MixinNetworkManagerChInit.java
@@ -0,0 +1,31 @@
+package de.enzaxd.viaforge.injection.mixins;
+
+import com.viaversion.viaversion.api.connection.UserConnection;
+import com.viaversion.viaversion.connection.UserConnectionImpl;
+import com.viaversion.viaversion.protocol.ProtocolPipelineImpl;
+import de.enzaxd.viaforge.handler.CommonTransformer;
+import de.enzaxd.viaforge.handler.VRDecodeHandler;
+import de.enzaxd.viaforge.handler.VREncodeHandler;
+import io.netty.channel.Channel;
+import io.netty.channel.socket.SocketChannel;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(targets = "net.minecraft.network.NetworkManager$5")
+public abstract class MixinNetworkManagerChInit {
+
+    @Inject(method = "initChannel", at = @At(value = "TAIL"), remap = false)
+    private void onInitChannel(Channel channel, CallbackInfo ci) {
+        if (channel instanceof SocketChannel) {
+
+            UserConnection user = new UserConnectionImpl(channel, true);
+            new ProtocolPipelineImpl(user);
+
+            channel.pipeline()
+                    .addBefore("encoder", CommonTransformer.HANDLER_ENCODER_NAME, new VREncodeHandler(user))
+                    .addBefore("decoder", CommonTransformer.HANDLER_DECODER_NAME, new VRDecodeHandler(user));
+        }
+    }
+}
