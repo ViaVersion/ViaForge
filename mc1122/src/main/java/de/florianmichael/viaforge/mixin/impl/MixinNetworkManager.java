63a8292966e07d3ac51e629bc67818f36104cab1
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-12-27 17:14:56 +0100

    updated VPH

---
+++ mc1122/src/main/java/de/florianmichael/viaforge/mixin/impl/MixinNetworkManager.java
@@ -0,0 +1,21 @@
+package de.florianmichael.viaforge.mixin.impl;
+
+import de.florianmichael.viaprotocolhack.event.PipelineReorderEvent;
+import io.netty.channel.Channel;
+import net.minecraft.network.NetworkManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(NetworkManager.class)
+public class MixinNetworkManager {
+
+    @Shadow private Channel channel;
+
+    @Inject(method = "setCompressionThreshold", at = @At("RETURN"))
+    public void reOrderPipeline(int p_setCompressionTreshold_1_, CallbackInfo ci) {
+        channel.pipeline().fireUserEventTriggered(new PipelineReorderEvent());
+    }
+}
