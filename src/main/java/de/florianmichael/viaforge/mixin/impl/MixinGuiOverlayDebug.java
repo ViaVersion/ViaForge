b4b95a2f9ee4b585f81bd55e93adac9e4a628056
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2024-03-11 14:03:46 +0100

    24w09a, port to upstream changes

--- src/main/java/de/florianmichael/viaforge/mixin/impl/MixinGuiOverlayDebug.java
+++ src/main/java/de/florianmichael/viaforge/mixin/impl/MixinGuiOverlayDebug.java
@@ -39,15 +39,7 @@ public class MixinGuiOverlayDebug {
 
         if (common.getConfig().isShowProtocolVersionInF3() && version != common.getNativeVersion() && !common.getPlatform().isSingleplayer().get()) {
             cir.getReturnValue().add("");
-
-            int protocolVersion = version.getVersion();
-            if (version.olderThanOrEqualTo(LegacyProtocolVersion.r1_6_4)) {
-                // Older versions (<= 1.6.4) are using fake ids in ViaLegacy to prevent version duplications / mismatches
-                // So we need to unmap the version to get the real protocol version id
-                protocolVersion = LegacyProtocolVersion.getRealProtocolVersion(protocolVersion);
-            }
-
-            cir.getReturnValue().add("ViaForge: " + version.getName() + " (" + protocolVersion + ")");
+            cir.getReturnValue().add("ViaForge: " + version.toString());
         }
     }
 
