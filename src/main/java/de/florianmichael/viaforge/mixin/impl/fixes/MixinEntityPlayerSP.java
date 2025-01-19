bf53e7db65a84143634161ee248d5ea515ad4a40
Howard ZHY <96782166+HowardZHY@users.noreply.github.com>
2025-01-19 17:54:30 +0800

    Temporary fix downgrading issue with JavaDowngrader (#152)

--- src/main/java/de/florianmichael/viaforge/mixin/impl/fixes/MixinEntityPlayerSP.java
+++ src/main/java/de/florianmichael/viaforge/mixin/impl/fixes/MixinEntityPlayerSP.java
@@ -27,7 +27,6 @@ import net.minecraft.client.network.NetHandlerPlayClient;
 import net.minecraft.network.Packet;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
