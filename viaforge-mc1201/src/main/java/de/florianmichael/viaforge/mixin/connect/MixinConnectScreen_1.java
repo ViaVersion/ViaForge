f30e8c24c5c6e5abc9e799590b72049ee9647824
Howard ZHY <howardzhy128@163.com>
2025-01-20 21:03:31 +0800

    Fix Java version and 1.20.1 support
    Both 1.20.1 Forge & NeoForge are tested
    
    Update README.md

--- viaforge-mc1201/src/main/java/de/florianmichael/viaforge/mixin/connect/MixinConnectScreen_1.java
+++ viaforge-mc1201/src/main/java/de/florianmichael/viaforge/mixin/connect/MixinConnectScreen_1.java
@@ -18,16 +18,20 @@
 
 package de.florianmichael.viaforge.mixin.connect;
 
+import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
 import de.florianmichael.viaforge.common.ViaForgeCommon;
 import de.florianmichael.viaforge.common.gui.ExtendedServerData;
 import de.florianmichael.viaforge.common.platform.VersionTracker;
-import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.ConnectScreen;
 import net.minecraft.client.multiplayer.ServerData;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.client.multiplayer.resolver.ServerAddress;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.net.InetSocketAddress;
 import java.util.Optional;
@@ -35,12 +39,19 @@ import java.util.Optional;
 @Mixin(targets = "net.minecraft.client.gui.screens.ConnectScreen$1")
 public class MixinConnectScreen_1 {
 
-    @Shadow @Final ServerData val$p_252078_;
+    @Unique
+    private ServerData viaforge$server;
 
+    @Inject(method = "<init>", at = @At("RETURN"))
+    public void init(ConnectScreen this$0, String arg0, ServerAddress par3, Minecraft par4, ServerData par5, CallbackInfo ci) {
+        viaforge$server = par5;
+    }
+
+    @SuppressWarnings("all")
     @Redirect(method = "run", at = @At(value = "INVOKE", target = "Ljava/util/Optional;get()Ljava/lang/Object;"))
     public Object trackServerVersion(Optional instance) {
         final InetSocketAddress address = (InetSocketAddress) instance.get();
-        ProtocolVersion version = ((ExtendedServerData) val$p_252078_).viaForge$getVersion();
+        ProtocolVersion version = ((ExtendedServerData) viaforge$server).viaForge$getVersion();
         if (version == null) {
             version = ViaForgeCommon.getManager().getTargetVersion();
         }
