5e3efb271586daf466d6193ff423dd8b42169a82
FlorianMichael <florian.michael07@gmail.com>
2025-06-30 23:59:40 +0200

    Update Via* libraries

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 </div>
 
 ## Supported Server versions
-- Release (1.0.0 - 1.21.5)
+- Release (1.0.0 - 1.21.7)
 - Beta (b1.0 - b1.8.1)
 - Alpha (a1.0.15 - a1.2.6)
 - Classic (c0.0.15 - c0.30 including [CPE](https://wiki.vg/Classic_Protocol_Extension))
@@ -27,8 +27,8 @@ For more details on how to use this feature, see the [Player Details Protocol wi
 
 ## How to (Users)
 
-ViaForge can be installed on 1.8.9, 1.12.2, 1.16.5, 1.17.1, 1.18.2, 1.19.2, 1.19.4, 1.20.1, 1.20.4, 1.20.6 and 1.21.5 Forge,
-and 1.20.1, 1.20.6, 1.21.5 NeoForge client versions. <br>
+ViaForge can be installed on 1.8.9, 1.12.2, 1.16.5, 1.17.1, 1.18.2, 1.19.2, 1.19.4, 1.20.1, 1.20.4, 1.20.6 and 1.21.7 Forge,
+and 1.20.1, 1.20.6, 1.21.7 NeoForge client versions. <br>
 
 Dev builds: https://ci.viaversion.com/view/Platforms/job/ViaForge/
 
--- viaforge-mc1194/src/main/java/de/florianmichael/viaforge/provider/ViaForgeGameProfileFetcher.java
+++ viaforge-mc1194/src/main/java/de/florianmichael/viaforge/provider/ViaForgeGameProfileFetcher.java
@@ -26,21 +26,21 @@ import com.mojang.authlib.minecraft.MinecraftSessionService;
 import com.mojang.authlib.properties.Property;
 import com.mojang.authlib.yggdrasil.ProfileNotFoundException;
 import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
-import net.raphimc.vialegacy.protocol.release.r1_7_6_10tor1_8.model.GameProfile;
-import net.raphimc.vialegacy.protocol.release.r1_7_6_10tor1_8.provider.GameProfileFetcher;
-
+import com.viaversion.viaversion.api.minecraft.GameProfile;
 import java.net.Proxy;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import net.raphimc.vialegacy.protocol.release.r1_7_6_10tor1_8.provider.GameProfileFetcher;
 
 public class ViaForgeGameProfileFetcher extends GameProfileFetcher {
 
-    public static final HttpAuthenticationService AUTHENTICATION_SERVICE = new YggdrasilAuthenticationService(Proxy.NO_PROXY, "");
-    public static final MinecraftSessionService SESSION_SERVICE = AUTHENTICATION_SERVICE.createMinecraftSessionService();
-    public static final GameProfileRepository GAME_PROFILE_REPOSITORY = AUTHENTICATION_SERVICE.createProfileRepository();
+    private static final HttpAuthenticationService AUTHENTICATION_SERVICE = new YggdrasilAuthenticationService(Proxy.NO_PROXY, "");
+    private static final MinecraftSessionService SESSION_SERVICE = AUTHENTICATION_SERVICE.createMinecraftSessionService();
+    private static final GameProfileRepository GAME_PROFILE_REPOSITORY = AUTHENTICATION_SERVICE.createProfileRepository();
 
     @Override
-    public UUID loadMojangUUID(String playerName) throws Exception {
+    public UUID loadMojangUuid(String playerName) throws Exception {
         final CompletableFuture<com.mojang.authlib.GameProfile> future = new CompletableFuture<>();
         GAME_PROFILE_REPOSITORY.findProfilesByNames(new String[]{playerName}, Agent.MINECRAFT, new ProfileLookupCallback() {
             @Override
@@ -60,18 +60,19 @@ public class ViaForgeGameProfileFetcher extends GameProfileFetcher {
     }
 
     @Override
-    public GameProfile loadGameProfile(UUID uuid) throws Exception {
+    public GameProfile loadGameProfile(UUID uuid) {
         final com.mojang.authlib.GameProfile inProfile = new com.mojang.authlib.GameProfile(uuid, null);
         final com.mojang.authlib.GameProfile mojangProfile = SESSION_SERVICE.fillProfileProperties(inProfile, true);
-        if (mojangProfile.equals(inProfile)) throw new ProfileNotFoundException();
-
-        final GameProfile gameProfile = new GameProfile(mojangProfile.getName(), mojangProfile.getId());
-        for (final java.util.Map.Entry<String, Property> entry : mojangProfile.getProperties().entries()) {
-            final Property prop = entry.getValue();
-            gameProfile.addProperty(new GameProfile.Property(prop.getName(), prop.getValue(), prop.getSignature()));
+        if (mojangProfile.equals(inProfile)) {
+            throw new ProfileNotFoundException();
         }
 
-        return gameProfile;
+        final GameProfile.Property[] properties = new GameProfile.Property[mojangProfile.getProperties().size()];
+        int i = 0;
+        for (final Map.Entry<String, Property> entry : mojangProfile.getProperties().entries()) {
+            properties[i++] = new GameProfile.Property(entry.getValue().getName(), entry.getValue().getValue(), entry.getValue().getSignature());
+        }
+        return new GameProfile(mojangProfile.getName(), mojangProfile.getId(), properties);
     }
 
 }
