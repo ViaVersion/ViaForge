1b9f6c747dea479e9ea296f0cf91957f12def121
FlorianMichael <git@florianmichael.de>
2026-02-01 11:10:54 +0100

    Workaround mods injecting the server status connection
    Fixes https://github.com/ViaVersion/ViaForge/issues/240
    
    Signed-off-by: FlorianMichael <git@florianmichael.de>

--- viaforge-mc1165/src/main/java/de/florianmichael/viaforge/mixin/connect/MixinServerPinger.java
+++ viaforge-mc1165/src/main/java/de/florianmichael/viaforge/mixin/connect/MixinServerPinger.java
@@ -18,21 +18,20 @@
 
 package de.florianmichael.viaforge.mixin.connect;
 
+import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
 import de.florianmichael.viaforge.common.ViaForgeCommon;
 import de.florianmichael.viaforge.common.extended.ExtendedServerData;
 import de.florianmichael.viaforge.common.platform.VersionTracker;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import net.minecraft.client.multiplayer.ServerAddress;
 import net.minecraft.client.multiplayer.ServerData;
 import net.minecraft.client.network.ServerPinger;
-import net.minecraft.network.NetworkManager;
-import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.net.InetAddress;
 
 @Mixin(ServerPinger.class)
 public class MixinServerPinger {
@@ -45,16 +44,15 @@ public class MixinServerPinger {
         viaForge$serverData = server;
     }
 
-    @Redirect(method = "pingServer", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/NetworkManager;connectToServer(Ljava/net/InetAddress;IZ)Lnet/minecraft/network/NetworkManager;"))
-    public NetworkManager trackVersion(InetAddress address, int i, boolean b) {
+    @Inject(method = "pingServer", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/NetworkManager;connectToServer(Ljava/net/InetAddress;IZ)Lnet/minecraft/network/NetworkManager;"))
+    public void trackVersion(ServerData p_147224_1_, Runnable p_147224_2_, CallbackInfo ci) throws UnknownHostException {
         ProtocolVersion version = ((ExtendedServerData) viaForge$serverData).viaForge$getVersion();
         if (version == null) {
             version = ViaForgeCommon.getManager().getTargetVersion();
         }
-        VersionTracker.storeServerProtocolVersion(address, version);
+        // Workaround for GH-240 where another mod is also redirecting the connect function
+        VersionTracker.storeServerProtocolVersion(InetAddress.getByName(ServerAddress.parseString(p_147224_1_.ip).getHost()), version);
         viaForge$serverData = null;
-
-        return NetworkManager.connectToServer(address, i, b);
     }
 
 }
